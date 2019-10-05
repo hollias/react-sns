896645cabe8c16604537e8d3fe6d8011c1c29799
hollias <clous.namsoo@hotmail.com>
2019-10-05 22:28:06 +0900

    add hashtag

---
+++ front/pages/hashtag.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { LOAD_HASHTAG_POSTS_REQUEST } from '../reducers/post';
+
+const Hashtag = ({ tag }) => {
+    const dispatch = useDispatch();
+    const { mainPosts } = useSelector(state => state.post);
+
+    useEffect(() => {
+        dispatch({
+        type: LOAD_HASHTAG_POSTS_REQUEST,
+        data: tag,
+        });
+    }, []);
+    return (
+        <div>
+        {mainPosts.map(c => (
+            <PostCard key={+c.createdAt} post={c} />
+        ))}
+        </div>
+    );
+}
+
+Hashtag.propTypes = {
+    tag: PropTypes.string.isRequired,
+};
+
+Hashtag.getInitialProps = async(context) => {
+    console.log('hashtag getInitialProps', context.query.tag);
+    return {
+        tag: context.query.tag
+    }
+}
+
+export default Hashtag;
---
+++ front/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const next = require('next');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const expressSession = require('express-session');
+
+const dev = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+dotenv.config();
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.use(morgan('dev'));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true}));
+    server.use(cookieParser(process.env.COOKIE_SECRET));
+    server.use(expressSession({
+        resave: false,
+        saveUninitalized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false
+        },
+    }));
+
+    server.get('/hashtag/:tag', (req, res) => {
+        return app.render(req, res, '/hashtag', { tag: req.params.tag });
+    });
+
+    server.get('/user/:id', (req, res) => {
+        return app.render(req, res, '/user', { id: req.params.id });
+    });
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    })
+
+    server.listen(3000, () => {
+        console.log('server is running on http://localhost:3000');
+    })
+});
